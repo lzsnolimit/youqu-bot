d2f3d7e3d2fc24ff7de56dc5c21c3fee80f27635
lzsnolimit <lzsnolimit@gmail.com>
2023-08-03 22:51:26 +0800

    add tss

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import asyncio
+
 from common import log
 import config
+from common.tss import edge_tss
 from speech_sample import speech_recognize_keyword_locally_from_microphone, speech_recognize_async_from_file, \
     speech_recognize_once_from_mic
 
@@ -8,7 +11,7 @@ if __name__ == '__main__':
         # load config
         config.load_config()
         query=speech_recognize_keyword_locally_from_microphone()
-
+        asyncio.run(edge_tss(query))
     except Exception as e:
         log.error("App startup failed!")
         log.exception(e)
--- common/tss.py
+++ common/tss.py
@@ -1,5 +1,20 @@
+import os
+import uuid
+
 import edge_tts
 
+from common import log
+from config import azure_conf
+
+
 async def edge_tss(TEXT) -> str:
-    communicate = edge_tts.Communicate(TEXT, VOICE)
-    await communicate.save(OUTPUT_FILE)
+
+    file_path = os.path.join(azure_conf("temp_file_path"), uuid.uuid4().hex + ".mp3")
+    voice = azure_conf("voice_name")
+    log.info("TTS start! text:{} voice:{}", TEXT, voice)
+    tts = edge_tts.Communicate(text=TEXT, voice=voice)
+
+
+    log.info("TTS finished! path:{}", file_path)
+    tts.save(file_path)
+    return file_path
